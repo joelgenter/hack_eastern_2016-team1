31df8f17297ca01f7cbad221c4ff6af828e4ced7
joelgenter <joelgenter@outlook.com>
2016-12-03 22:52:55 -0500

    changes

--- app.js
+++ app.js
@@ -28,6 +28,13 @@ io.on('connection', function(player) {
 		mousey: 300
 	};
 
+	player.keysDown = {
+		37: false,
+		38: false,
+		39: false,
+		40: false
+	};
+
 	player.emit('initializePlayer', thisPlayer);
 	SOCKET_LIST[player.id] = player;
 	PLAYER_LIST[player.id] = thisPlayer;
@@ -36,14 +43,7 @@ io.on('connection', function(player) {
 	//handle the event of a user changing key input
 	//@param keysDown the object representing which keys are pressed down
 	player.on('keyInput', function(keysDown) {
-		if (keysDown[37]) //37 is left
-			PLAYER_LIST[player.id].x -= playerSpeed;
-		if (keysDown[39]) //39 is right
-			PLAYER_LIST[player.id].x += playerSpeed;
-		if (keysDown[38]) //38 is up
-			PLAYER_LIST[player.id].y += playerSpeed;
-		if (keysDown[40]) //40 is down
-			PLAYER_LIST[player.id].y -= playerSpeed;
+		SOCKET_LIST[player.id].keysDown = keysDown;
 	});
 
 
@@ -56,6 +56,18 @@ io.on('connection', function(player) {
 });
 
 setInterval(function() {
+	for (var i in SOCKET_LIST) {
+		var currentPlayer = SOCKET_LIST[i];
+		if (currentPlayer.keysDown[37]) //37 is left
+			PLAYER_LIST[player.id].x -= playerSpeed;
+		if (currentPlayer.keysDown[39]) //39 is right
+			PLAYER_LIST[player.id].x += playerSpeed;
+		if (currentPlayer.keysDown[38]) //38 is up
+			PLAYER_LIST[player.id].y += playerSpeed;
+		if (currentPlayer.keysDown[40]) //40 is down
+			PLAYER_LIST[player.id].y -= playerSpeed;
+	}
+
 	for (var i in SOCKET_LIST) {
 		var currentPlayer = SOCKET_LIST[i];
 		currentPlayer.emit('gameStateChange', PLAYER_LIST);
